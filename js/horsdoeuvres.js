1556d3ce177b18209312e2424c816cff3761d9d3
Emily Allen <emmylou522@gmail.com>
2015-04-15 19:23:29 -0400

    fix js to accommodate for conflicting plugins

--- js/horsdoeuvres.js
+++ js/horsdoeuvres.js
@@ -1,44 +1,74 @@
-$(document).ready( function() {
-    //sticky plug in//
-    $('.sneakyBlock').sticky();
-    //scrollUp plug in//
+function createMenu() {
+    if ($('#logo').css("width") !== "190px") {
+        //Check if menu exists.  If it does, remove the current menu.
+        if (document.getElementById("menu")) {
+        //remove the current one
+        $('#menu').remove();    
+        }
+    //then, add a new one since the plugin messed up the menu.
+    $('header').append('<nav id="menu"><ul><li><a href="#">Menu</a><ul class="fallback"><li><a href="horsdoeuvres.html">Hors D\'oeuvres</a></li><li><a href="entrees.html">Entrees</a></li><li><a href="desserts.html">Desserts</a></li><li><a href="wines.html">Wines</a></li></ul></li><li><a href="index.html#reservationlinks">Make a Reservation</a></li><li><a href="order.html">Order Out</a></li><li><a href="contact.html">Contact Us</a></li><li><a href="index.html">Home</a></li></ul></nav>');
+    //Stretch the image first.
+    $.backstretch("img/bread.jpg");
+    //Run the jbar plugin
+    // jbar plugin
+    $('nav li ul').hide().removeClass('fallback');
+    $('nav li').hover(function () { $('ul', this).stop().slideDown(100);}, function () { $('ul', this).stop().slideUp(100); });
+    //end jbar plugin
+    }
+    //If it isn't desktop.
+    else {
+        //Check if menu exists.  If it does, remove the current menu.
+        if (document.getElementById("menu")) {
+            //remove the current one
+            $('#menu').remove();    
+        }
+        $('header').append('<nav id="menu"><ul><li><a href="#">Menu</a><ul class="fallback"><li><a href="horsdoeuvres.html">Hors D\'oeuvres</a></li><li><a href="entrees.html">Entrees</a></li><li><a href="desserts.html">Desserts</a></li><li><a href="wines.html">Wines</a></li></ul></li><li><a href="index.html#reservationlinks">Make a Reservation</a></li><li><a href="order.html">Order Out</a></li><li><a href="contact.html">Contact Us</a></li><li><a href="index.html">Home</a></li></ul></nav>');
+
+    //Stretch the image first.
+    $.backstretch("img/bread.jpg");
+    //scrollUp plug in
     $.scrollUp();
-    $("nav#menu").mmenu({
-      //options
+    //end scrollUp plugin
+    //begin navmenu plugin
+    $("nav#menu").mmenu({    
+        //options
         // A collection of extension names to enable for the menu.
-      extensions    : [],
-      onClick     : {
-  // the clicked link should be visibly "selected".
-      setSelected   : true
-},
-// the submenus comes sliding in from the right.
-      slidingSubmenus : true
-    }, {
+        extensions    : [],
+        onClick     : {
+        // the clicked link should be visibly "selected".
+        setSelected   : true
+        },
+        // the submenus comes sliding in from the right.
+        slidingSubmenus : true
+        }, {
         configuration: {
-      // CSS classesclassNames: {
+        // CSS classesclassNames: {
         panel   : 'Panel',
         vertical  : 'Vertical',
         selected  : 'Selected',
         divider   : 'Divider',
         spacer    : 'Spacer'
-},
- 
-// Whether or not the menu should be cloned
-// (and the original menu kept intact).
-      clone: false,
-// The number of milliseconds between opening/closing the menu and panels,
-// needed to force CSS transitions.
-      openingInterval: 25,
-// jQuery selector containing the node-type of panels.
-      panelNodetype: 'ul, ol, div',
-// The number of milliseconds used in the CSS transitions.
-      transitionDuration: 400
-   }
-);
+        },
+        // Whether or not the menu should be cloned
+        // (and the original menu kept intact).
+        clone: false,
+        // The number of milliseconds between opening/closing the menu and panels,
+        // needed to force CSS transitions.
+        openingInterval: 25,
+        // jQuery selector containing the node-type of panels.
+        panelNodetype: 'ul, ol, div',
+        // The number of milliseconds used in the CSS transitions.
+        transitionDuration: 400
+        }
+    );
+        
     $(".mm-menu").trigger("open");
-    $(".mm-menu").trigger("close");
+    $(".mm-menu").trigger("close"); 
+    //end navmenu plugin
+    //Show the menu.
+    }
+}
 
-//backstretch plugin //
-    $.backstretch("img/bread.jpg");
+$(document).ready(function() {createMenu(); });
 
-});
+$(window).resize(function() {createMenu(); });
